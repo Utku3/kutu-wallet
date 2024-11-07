a4de181486bc137c3e89c3a62efddab7c5f8fb04
Utku3 <72941544+Utku3@users.noreply.github.com>
2024-11-07 19:22:28 +0100

    Config modified

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'expenses.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = 'df1/2Koo.9pL%F'
+SECRET_KEY = 'df1/2Koo.9pL%F' # random
